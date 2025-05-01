d1448a57b35b475271c49495b42f6d3a3b484ff8
Ni8mare1997 <dudehimanshu97@gmail.com>
2025-05-01 11:57:09 +0530

    Pta nhi bhai

--- src/Layout/contentLayout.jsx
+++ src/Layout/contentLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export function ContentLayout() {
   return (
     <div className="box-border h-full col-span-2 row-span-12 row-start-2 grid grid-cols-12 grid-rows-12 gap-1">
-    <Outlet/>
+      <Outlet />
     </div>
   );
 }
--- src/pages/maincomponent.jsx
+++ src/pages/maincomponent.jsx
@@ -1,10 +1,13 @@
 import { ContentLayout } from "../Layout/contentLayout";
 import { NavBar } from "../Components/navBar";
 import { useSelector } from "react-redux";
+import { Loading } from "../Components/loading";
 
 export function MainComp() {
   const { userInfo } = useSelector((state) => state.auth);
-
+  if (!userInfo) {
+    return <Loading />;
+  }
   return (
     <>
       <NavBar />
